6bcfb5ce8ccafcb722ee1d63f5c920feb92207d0
Thomas Whalen-Bridge <thomasjmwb@gmail.com>
2025-09-06 18:52:06 +0100

    GAMING

--- Assets/player.cs
+++ Assets/player.cs
@@ -3,6 +3,7 @@ using UnityEngine;
 public class player : MonoBehaviour
 {
     public Rigidbody2D rb;
+    public float speed = 5f;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -12,17 +13,16 @@ public class player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.Space))
-        {
-            rb.linearVelocity = Vector2.up * 5;
-        }
-        if(Input.GetKeyDown(KeyCode.A))
-        {
-            rb.linearVelocity = Vector2.left * 5;
-        }
-        if(Input.GetKeyDown(KeyCode.D))
-        {
-            rb.linearVelocity = Vector2.right * 5;
-        }
+        
+            if(Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.Space))
+            {
+                rb.AddForce(new Vector3(0, speed), ForceMode2D.Impulse);
+            }
+
+        float horizontal = 0f;
+        if (Input.GetKey(KeyCode.A)) horizontal -= speed;
+        if (Input.GetKey(KeyCode.D)) horizontal += speed;
+
+        transform.Translate(new Vector3(horizontal * Time.deltaTime, 0, 0));
     }
 }
